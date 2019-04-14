a27147b6fade86e85299814c77be93f8b267bee6
felixbrinadze <felix.brinadze@gmail.com>
2019-04-14 15:25:18 -0400

    Commit blah blach

--- testJS.js
+++ testJS.js
@@ -1,3 +1,9 @@
 var helloWorld = "Hello World, from me ";
-var username = "fbrinadze";
+var username = "fbrinadze.";
 console.log(helloWorld + username);
+var fTemp = 61;
+var cTemp;
+cTemp = (fTemp - 32) * (5/9);
+console.log(cTemp.toFixed(2));
+fTemp = (cTemp * 1.8) + 32;
+document.write(fTemp);
